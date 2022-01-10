b7a59f6bfddf0d614997bcadd86fb454668bfb0c
Mary-h86 <msh588@nyu.edu>
2022-01-10 16:09:05 -0500

    update train.py

--- train.py
+++ train.py
@@ -35,6 +35,8 @@ import os
 import sys
 import time
 import torch
+import numpy as np
+import pandas as pd
 
 from apex import amp
 from runx.logx import logx
@@ -44,9 +46,11 @@ from utils.misc import ImageDumper
 from utils.trnval_utils import eval_minibatch, validate_topn
 from loss.utils import get_loss
 from loss.optimizer import get_optimizer, restore_opt, restore_net
-
+#using the synchbatch
+from syncbatch.sync_batchnorm import convert_model
 import datasets
 import network
+from torch.utils.tensorboard import SummaryWriter
 
 
 # Import autoresume module
@@ -61,9 +65,9 @@ except ImportError:
 # Argument Parser
 parser = argparse.ArgumentParser(description='Semantic Segmentation')
 parser.add_argument('--lr', type=float, default=0.002)
-parser.add_argument('--arch', type=str, default='deepv3.DeepWV3Plus',
+parser.add_argument('--arch', type=str, default='ocrnet.HRNet_Mscale',
                     help='Network architecture. We have DeepSRNX50V3PlusD (backbone: ResNeXt50) \
-                    and deepWV3Plus (backbone: WideResNet38).')
+                    and deepV3Plus (backbone: WideResNet38).')
 parser.add_argument('--dataset', type=str, default='cityscapes',
                     help='cityscapes, mapillary, camvid, kitti')
 parser.add_argument('--dataset_inst', default=None,
@@ -77,7 +81,7 @@ parser.add_argument('--cv', type=int, default=0,
 
 parser.add_argument('--class_uniform_pct', type=float, default=0.5,
                     help='What fraction of images is uniformly sampled')
-parser.add_argument('--class_uniform_tile', type=int, default=1024,
+parser.add_argument('--class_uniform_tile', type=int, default=320,
                     help='tile size for class uniform sampling')
 parser.add_argument('--coarse_boost_classes', type=str, default=None,
                     help='Use coarse annotations for specific classes')
@@ -85,11 +89,11 @@ parser.add_argument('--coarse_boost_classes', type=str, default=None,
 parser.add_argument('--custom_coarse_dropout_classes', type=str, default=None,
                     help='Drop some classes from auto-labelling')
 
-parser.add_argument('--img_wt_loss', action='store_true', default=False,
+parser.add_argument('--img_wt_loss', action='store_true', default=True,
                     help='per-image class-weighted loss')
 parser.add_argument('--rmi_loss', action='store_true', default=False,
                     help='use RMI loss')
-parser.add_argument('--batch_weighting', action='store_true', default=False,
+parser.add_argument('--batch_weighting', action='store_true', default=True,
                     help=('Batch weighting for class (use nll class weighting using '
                           'batch stats'))
 
@@ -122,7 +126,7 @@ parser.add_argument('--hardnm', default=0, type=int,
                     help=('0 means no aug, 1 means hard negative mining '
                           'iter 1, 2 means hard negative mining iter 2'))
 
-parser.add_argument('--trunk', type=str, default='resnet101',
+parser.add_argument('--trunk', type=str, default='hrnetv2',
                     help='trunk model, can be: resnet101 (default), resnet50')
 parser.add_argument('--max_epoch', type=int, default=180)
 parser.add_argument('--max_cu_epoch', type=int, default=150,
@@ -132,7 +136,7 @@ parser.add_argument('--color_aug', type=float,
                     default=0.25, help='level of color augmentation')
 parser.add_argument('--gblur', action='store_true', default=False,
                     help='Use Guassian Blur Augmentation')
-parser.add_argument('--bblur', action='store_true', default=False,
+parser.add_argument('--bblur', action='store_true', default=True,
                     help='Use Bilateral Blur Augmentation')
 parser.add_argument('--brt_aug', action='store_true', default=False,
                     help='Use brightness augmentation')
@@ -146,7 +150,7 @@ parser.add_argument('--bs_trn', type=int, default=2,
                     help='Batch size for training per gpu')
 parser.add_argument('--bs_val', type=int, default=1,
                     help='Batch size for Validation per gpu')
-parser.add_argument('--crop_size', type=str, default='896',
+parser.add_argument('--crop_size', type=str, default='640,640',
                     help=('training crop size: either scalar or h,w'))
 parser.add_argument('--scale_min', type=float, default=0.5,
                     help='dynamically scale training images down to this size')
@@ -154,7 +158,7 @@ parser.add_argument('--scale_max', type=float, default=2.0,
                     help='dynamically scale training images up to this size')
 parser.add_argument('--weight_decay', type=float, default=1e-4)
 parser.add_argument('--momentum', type=float, default=0.9)
-parser.add_argument('--snapshot', type=str, default=None)
+parser.add_argument('--snapshot', type=str, default='/scratch/msh588/spec-list/Nvidia-attention/seg_weights/best_checkpoint_ep74.pth')
 parser.add_argument('--resume', type=str, default=None,
                     help=('continue training from a checkpoint. weights, '
                           'optimizer, schedule are restored'))
@@ -162,7 +166,7 @@ parser.add_argument('--restore_optimizer', action='store_true', default=False)
 parser.add_argument('--restore_net', action='store_true', default=False)
 parser.add_argument('--exp', type=str, default='default',
                     help='experiment directory name')
-parser.add_argument('--result_dir', type=str, default='./logs',
+parser.add_argument('--result_dir', type=str, default='/scratch/msh588/spec-list/validation-results/DC',
                     help='where to write log output')
 parser.add_argument('--syncbn', action='store_true', default=False,
                     help='Use Synchronized BN')
@@ -194,7 +198,7 @@ parser.add_argument('--log_msinf_to_tb', action='store_true', default=False,
 parser.add_argument('--eval', type=str, default=None,
                     help=('just run evaluation, can be set to val or trn or '
                           'folder'))
-parser.add_argument('--eval_folder', type=str, default=None,
+parser.add_argument('--eval_folder', type=str, default="/scratch/msh588/spec-list/data_test/washington_dc",
                     help='path to frames to evaluate')
 parser.add_argument('--three_scale', action='store_true', default=False)
 parser.add_argument('--alt_two_scale', action='store_true', default=False)
@@ -215,11 +219,11 @@ parser.add_argument('--rand_augment', default=None,
                     help='RandAugment setting: set to \'N,M\'')
 parser.add_argument('--init_decoder', default=False, action='store_true',
                     help='initialize decoder with kaiming normal')
-parser.add_argument('--dump_topn', type=int, default=0,
+parser.add_argument('--dump_topn', type=int, default=10,
                     help='Dump worst val images')
 parser.add_argument('--dump_assets', action='store_true',
                     help='Dump interesting assets')
-parser.add_argument('--dump_all_images', action='store_true',
+parser.add_argument('--dump_all_images', action='store_true',default=True,
                     help='Dump all images, not just a subset')
 parser.add_argument('--dump_for_submission', action='store_true',
                     help='Dump assets for submission')
@@ -280,24 +284,30 @@ torch.backends.cudnn.benchmark = True
 if args.deterministic:
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-
-args.world_size = 1
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+args.world_size = 4
 
 # Test Mode run two epochs with a few iterations of training and val
 if args.test_mode:
     args.max_epoch = 2
 
-if 'WORLD_SIZE' in os.environ and args.apex:
-    # args.apex = int(os.environ['WORLD_SIZE']) > 1
-    args.world_size = int(os.environ['WORLD_SIZE'])
-    args.global_rank = int(os.environ['RANK'])
+#if 'WORLD_SIZE' in os.environ: #and args.apex:
+#    args.apex = int(os.environ['WORLD_SIZE']) > 1
+#if 'WORLD_SIZE' in os.environ:
+#    args.world_size = int(os.environ['WORLD_SIZE'])
+#    args.global_rank = int(os.environ['RANK'])
 
-if args.apex:
-    print('Global Rank: {} Local Rank: {}'.format(
-        args.global_rank, args.local_rank))
-    torch.cuda.set_device(args.local_rank)
-    torch.distributed.init_process_group(backend='nccl',
-                                         init_method='env://')
+
+#    print('Global Rank: {} Local Rank: {}'.format(args.global_rank, args.local_rank))
+#    torch.cuda.set_device(args.local_rank)
+#    torch.distributed.init_process_group(backend='nccl',
+#                                         init_method='env://')
+#if args.apex:	
+#    print('Global Rank: {} Local Rank: {}'.format(
+#        args.global_rank, args.local_rank))
+#    torch.cuda.set_device(args.local_rank)
+#    torch.distributed.init_process_group(backend='nccl',
+#                                         init_method='env://')
 
 
 def check_termination(epoch):
@@ -332,7 +342,7 @@ def main():
     logx.initialize(logdir=args.result_dir,
                     tensorboard=True, hparams=vars(args),
                     global_rank=args.global_rank)
-
+    print(args)
     # Set up the Arguments, Tensorboard Writer, Dataloader, Loss Fn, Optimizer
     assert_and_infer_cfg(args)
     prep_experiment(args)
@@ -373,22 +383,25 @@ def main():
         args.restore_net = True
         msg = "Loading weights from: checkpoint={}".format(args.snapshot)
         logx.msg(msg)
-
+#    print("criterion{}".format(criterion))
     net = network.get_net(args, criterion)
     optim, scheduler = get_optimizer(args, net)
 
     if args.fp16:
         net, optim = amp.initialize(net, optim, opt_level=args.amp_opt_level)
-
-    net = network.wrap_network_in_dataparallel(net, args.apex)
+    
+#    net = network.wrap_network_in_dataparallel(net, args.apex)
+    #using synchbatch 
+    net = torch.nn.DataParallel(net)
+    net = convert_model(net)    
 
     if args.summary:
-        print(str(net))
+#        print(str(net))
         from pytorchOpCounter.thop import profile
-        img = torch.randn(1, 3, 1024, 2048).cuda()
-        mask = torch.randn(1, 1, 1024, 2048).cuda()
+        img = torch.randn(1, 3, 640, 640).cuda()
+        mask = torch.randn(1, 1, 640, 640).cuda()
         macs, params = profile(net, inputs={'images': img, 'gts': mask})
-        print(f'macs {macs} params {params}')
+#        print(f'macs {macs} params {params}')
         sys.exit()
 
     if args.restore_optimizer:
@@ -409,22 +422,35 @@ def main():
     #  --eval val --dump_assets             dump all images and assets
     #  --eval folder                        just dump all basic images
     #  --eval folder --dump_assets          dump all images and assets
+    #  --eval test                          run inference only- dump all images 
+
+    if args.eval == 'test':
+        validate(val_loader, net, criterion=None, optim=None, epoch=0,
+                 calc_metrics=False, dump_assets=args.dump_assets,
+                 dump_all_images=True, testing=True)
+                 
+        return 0 
+
     if args.eval == 'val':
 
         if args.dump_topn:
             validate_topn(val_loader, net, criterion_val, optim, 0, args)
         else:
+
             validate(val_loader, net, criterion=criterion_val, optim=optim, epoch=0,
                      dump_assets=args.dump_assets,
                      dump_all_images=args.dump_all_images,
                      calc_metrics=not args.no_metrics)
         return 0
     elif args.eval == 'folder':
+   
         # Using a folder for evaluation means to not calculate metrics
-        validate(val_loader, net, criterion=None, optim=None, epoch=0,
-                 calc_metrics=False, dump_assets=args.dump_assets,
-                 dump_all_images=True)
+
+        #validate(val_loader, net, criterion=criterion_val, optim=optim, epoch=0,
+        #dump_assets=args.dump_assets, dump_all_images=args.dump_all_images)
+        validate_topn(val_loader, net, criterion_val, optim, 0, args)
         return 0
+
     elif args.eval is not None:
         raise 'unknown eval option {}'.format(args.eval)
 
@@ -453,7 +479,10 @@ def main():
         if args.apex:
             train_loader.sampler.set_epoch(epoch + 1)
 
+#        print("AAAAAAAAAAAAAAAAAAAA {},{}".format(epoch,args.val_freq))  
         if epoch % args.val_freq == 0:
+#            print("validation train - net {}, criterion_val {}, epoch {} ".format(net.state_dict(),criterion_val,epoch))
+           # print("we are in validation")  
             validate(val_loader, net, criterion_val, optim, epoch)
 
         scheduler.step()
@@ -488,6 +517,8 @@ def train(train_loader, net, optim, curr_epoch):
         inputs = {'images': images, 'gts': gts}
 
         optim.zero_grad()
+        
+        net = net.to(f'cuda:{net.device_ids[0]}')
         main_loss = net(inputs)
 
         if args.apex:
@@ -536,7 +567,7 @@ def train(train_loader, net, optim, curr_epoch):
 def validate(val_loader, net, criterion, optim, epoch,
              calc_metrics=True,
              dump_assets=False,
-             dump_all_images=False):
+             dump_all_images=False, testing=None):
     """
     Run validation for one epoch
 
@@ -554,13 +585,22 @@ def validate(val_loader, net, criterion, optim, epoch,
                          dump_assets=dump_assets,
                          dump_for_auto_labelling=args.dump_for_auto_labelling,
                          dump_for_submission=args.dump_for_submission)
-
+    
+    #new added line 
+    net = net.to(f'cuda:{net.device_ids[0]}')
     net.eval()
     val_loss = AverageMeter()
     iou_acc = 0
+    pred = dict()
+    #TENSORBOARD HERE
+    #tb = SummaryWriter()
 
     for val_idx, data in enumerate(val_loader):
+#        print('valindex: ', val_idx)
+#        print('data: ', data)
+
         input_images, labels, img_names, _ = data 
+        
         if args.dump_for_auto_labelling or args.dump_for_submission:
             submit_fn = '{}.png'.format(img_names[0])
             if val_idx % 20 == 0:
@@ -572,30 +612,58 @@ def validate(val_loader, net, criterion, optim, epoch,
         assets, _iou_acc = \
             eval_minibatch(data, net, criterion, val_loss, calc_metrics,
                           args, val_idx)
-
+        #print("vaidation function - assests{}".format(assets))
         iou_acc += _iou_acc
 
         input_images, labels, img_names, _ = data
 
-        dumper.dump({'gt_images': labels,
-                     'input_images': input_images,
-                     'img_names': img_names,
-                     'assets': assets}, val_idx)
+        if testing:
+            
+            #
+            prediction = assets['predictions'][0]
+            values, counts = np.unique(prediction, return_counts=True)
+                    #construct a dictionary with n keys for our class
+                    #dictin = {i:np.nan for i in range(1, cfg.DATASET_INST.num_classes+1)}
+                    #dictin.update(dict(zip(values, counts)))
+            pred.update({img_names[0]: dict(zip(values, counts))})
+            #df = pd.DataFrame.from_dict(pred, orient='index')
+            #df_p = df.div(df.sum(axis=1), axis=0)
+            #df_p.to_csv(os.path.join(dumper.save_dir, 'inTrain.csv'), mode = 'a+', header=False)
+            dumper.dump({'gt_images': labels,'input_images': input_images,'img_names': img_names, 'assets': assets}, val_idx, testing=True)
+            #pred = dumper.dump({'gt_images': labels,'input_images': input_images,'img_names': img_names, 'assets': assets}, val_idx, test_mode=True,give_dict=True)
+            #df = pd.DataFrame.from_dict(pred, orient='index')
+            #df_p = df.div(df.sum(axis=1), axis=0)
+            #df_p.to_csv(os.path.join(dumper.save_dir, 'inTrain.csv'), mode = 'a+', header=False)
+        else:
+            dumper.dump({'gt_images': labels, 'input_images': input_images,'img_names': img_names, 'assets': assets}, val_idx)
 
         if val_idx > 5 and args.test_mode:
             break
 
         if val_idx % 20 == 0:
             logx.msg(f'validating[Iter: {val_idx + 1} / {len(val_loader)}]')
-
+         
+        #tb.add_scalar('IoU',iou_acc)
+        #tb.add_scalar('val loss',val_loss) 
+    #tb.close()  
     was_best = False
     if calc_metrics:
         was_best = eval_metrics(iou_acc, args, net, optim, val_loss, epoch)
+    
+     
+    
+
 
     # Write out a summary html page and tensorboard image table
     if not args.dump_for_auto_labelling and not args.dump_for_submission:
         dumper.write_summaries(was_best)
+    if testing:
+        
+        df = pd.DataFrame.from_dict(pred, orient='index')
+        df_p = df.div(df.sum(axis=1), axis=0)
+        df_p.to_csv(os.path.join(dumper.save_dir,'freq.csv'), mode = 'a+', header=False)
 
 
 if __name__ == '__main__':
     main()
+
