642c9301ce8b7968c3b27378fc46bafb54608eeb
Mary-h86 <msh588@nyu.edu>
2022-01-10 22:12:02 -0500

    update create folders

--- datasets/utils.py
+++ datasets/utils.py
@@ -1,17 +1,29 @@
 import os
+import numpy as np
+from PIL import Image
 
 
-def make_dataset_folder(folder):
+def make_dataset_folder(folder, test_mode=None):
     """
     Create Filename list for images in the provided path
 
     input: path to directory with *only* images files
+	   test_mode: for test only with no ground truth 
+   
     returns: items list with None filled for mask path
     """
     items = os.listdir(folder)
-    items = [(os.path.join(folder, f), '') for f in items]
-    items = sorted(items)
+    if test_mode:
+                
+        items = [(os.path.join(folder, f), '') for f in items]
 
+    
+    else:
+
+        mask_root = folder.replace('images', 'annotations', 6)
+        items = [(os.path.join(folder, f), os.path.join(mask_root, f)) for f in items]
+    
+    items = sorted(items)
     print(f'Found {len(items)} folder imgs')
 
     """
