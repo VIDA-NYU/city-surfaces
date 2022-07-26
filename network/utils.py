3ccf0d646b56093d552dba24c73cb5d9afb4bb40
Dan Segal <danSegal2@gmail.com>
2022-07-26 22:24:56 +0200

    Remove unneeded network files from util load

--- network/utils.py
+++ network/utils.py
@@ -35,10 +35,7 @@ import torch.nn.functional as F
 from torch import nn
 
 from network.mynn import Norm2d, Upsample
-from network.xception import xception71
-from network.wider_resnet import wrn38
 from network.SEresnext import se_resnext50_32x4d, se_resnext101_32x4d
-from network.Resnet import resnet50, resnet101
 import network.hrnetv2 as hrnetv2
 
 from runx.logx import logx
